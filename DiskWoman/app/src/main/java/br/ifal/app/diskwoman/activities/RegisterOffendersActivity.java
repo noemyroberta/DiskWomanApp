e3bcc7ff206de3462d5e888ecd83d36bbfae8592
Rykelme Beorlegui <rykelmebeorlegui@gmail.com>
2019-12-06 16:53:50 -0300

    register offenders and register involved xml and activities added Commit

---
+++ DiskWoman/app/src/main/java/br/ifal/app/diskwoman/activities/RegisterOffendersActivity.java
@@ -0,0 +1,44 @@
+package br.ifal.app.diskwoman.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import br.ifal.app.diskwoman.beans.Offenders;
+import br.ifal.app.diskwoman.services.QueryTask;
+import br.ifal.app.diskwoman.R;
+
+import br.ifal.app.diskwoman.daos.OffendersDAO;
+
+
+public class RegisterOffendersActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.activity_register_offenders);
+
+    Intent intent = getIntent();
+    if(intent.hasExtra("offenders")){
+        EditText editTextName = findViewById(R.id.edittext_offe_name_id);
+        EditText editTextSex = findViewById(R.id.edittext_offe_sex_id);
+        EditText editTextAppearence = findViewById(R.id.edittext_offe_appearence_id);
+        EditText editTextAge = findViewById(R.id.edittext_offe_age_id);
+
+        Offenders offenders = (Offenders) intent.getSerializableExtra("offenders");
+        editTextName.setText(offenders.getName());
+        editTextSex.setText(offenders.getSex());
+        editTextAppearence.setText(offenders.getAppearence());
+        editTextAge.setText(offenders.getAge());
+        
+
+
+    }
+
+    }
+}
