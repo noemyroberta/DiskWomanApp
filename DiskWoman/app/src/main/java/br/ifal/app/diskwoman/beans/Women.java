5be40615a5cb4ee1f2380d2f5e2fa66bbd4ffa1c
Noemy Roberta <noemy.oliveira2@outlook.com>
2019-12-06 11:20:50 -0300

    Register Class and Services done

---
+++ DiskWoman/app/src/main/java/br/ifal/app/diskwoman/beans/Women.java
@@ -0,0 +1,53 @@
+package br.ifal.app.diskwoman.beans;
+
+import java.io.Serializable;
+
+public class Women implements Serializable {
+
+    private int id;
+    private String name;
+    private String email;
+
+    public Women(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Women() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Womans{" +
+                "Id =" + id +
+                ", name ='" + name + '\'' +
+                ", email ='" + email + '\'' +
+                '}';
+    }
+}
